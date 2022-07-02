5c10ed9cb7ac3736eaf2a35652cb32b5210dcb2e
Julieta Maldonado <80077281+JulietaMaldonado@users.noreply.github.com>
2022-07-02 17:08:45 -0300

    estilos lightMood y arreglos en todos los archivos

--- main.js
+++ main.js
@@ -1,1 +1,43 @@
+//DISPLAY TEXTO E IMAGEN ASIDE
+const asideImagen = document.getElementById("asideImagen");
+const asideTexto = document.getElementById("asideTexto");
 
+const textoBoton = document.getElementById("textoBoton");
+const imagenBoton = document.getElementById("imagenBoton");
+
+textoBoton.addEventListener("click", () => {
+  asideImagen.style.display = "none";
+  asideTexto.style.display = "block";
+});
+
+imagenBoton.addEventListener("click", () => {
+  asideImagen.style.display = "block";
+  asideTexto.style.display = "none";
+});
+
+//MODO LIGHT DE LA PAGINA
+const modoLight = document.getElementById("modo");
+
+modoLight.addEventListener("click", () => {
+  document.getElementById("contenedor").classList.toggle(`lightMood2`);
+  document.getElementById("encabezado").classList.toggle(`lightMood1`);
+  document.getElementById("aside").classList.toggle(`lightMood3`);
+  document.getElementById("imagenBoton").classList.toggle(`lightMood4:hover`);
+  document.getElementById("textoBoton").classList.toggle(`lightMood4:hover`);
+  document.getElementById("modo").classList.toggle(`lightMood4:hover`);
+  document.getElementsById("urlImagen").classList.toggle(`lightMood2`);
+  document.getElementsById("urlImagen").classList.toggle(`lightMood2`);
+  document.getElementsById("colorFondoImagen").classList.toggle(`lightMood2`);
+  document.getElementsById("modosFusion").classList.toggle(`lightMood2`);
+  document.getElementsById("restablecerBoton").classList.toggle(`lightMood2`);
+});
+
+//URL IMAGEN INPUT
+/*
+const urlImagen = document.getElementById("urlImagen");
+const meme = document.getElementById("meme");
+
+urlImagen.addEventListener("click", (event) => {
+  urlImagen.textContent = e.target.value;
+  meme.style.toggle.backgroundImage(urlImagen);
+});*/
