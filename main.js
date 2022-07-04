58d6e0e62eea6140359da7b67e22786c5aa7bc0c
Julieta Maldonado <80077281+JulietaMaldonado@users.noreply.github.com>
2022-07-03 22:21:09 -0300

    creacion y arreglo de funciones en js

--- main.js
+++ main.js
@@ -32,12 +32,61 @@ modoLight.addEventListener("click", () => {
   document.getElementsById("restablecerBoton").classList.toggle(`lightMood2`);
 });
 
+//FUNCIONES DEL ASIDE "IMAGEN"
+
 //URL IMAGEN INPUT
-/*
 const urlImagen = document.getElementById("urlImagen");
 const meme = document.getElementById("meme");
+urlImagen.addEventListener("input", () => {
+  meme.style = `background-image: url(${urlImagen.value})`;
+});
 
-urlImagen.addEventListener("click", (event) => {
-  urlImagen.textContent = e.target.value;
-  meme.style.toggle.backgroundImage(urlImagen);
+/*urlImagen.addEventListener("input", (event) => {
+  const valorActual = event.target.value;
+  meme.style.backgroundImage = url(valorActual);
 });*/
+
+//COLOR DE MEME INPUT
+const colorFondoImagen = document.getElementById("colorFondoImagen");
+
+colorFondoImagen.addEventListener("input", (event) => {
+  const valorActual = event.target.value;
+  meme.style.backgroundColor = valorActual;
+});
+
+//FUNCIONES DEL ASIDE "TEXTO"
+
+//INPUT DE TEXTO SUPERIOR E INFERIOR
+const textoTM = document.getElementById("textoTM");
+const textoBM = document.getElementById("textoBM");
+const textoTop = document.getElementById("textoTop");
+const textoBotton = document.getElementById("textoBotton");
+
+textoTop.addEventListener("input", (event) => {
+  const valorActual = event.target.value;
+  textoTM.innerHTML = valorActual;
+});
+
+textoBotton.addEventListener("input", (event) => {
+  const valorActual = event.target.value;
+  textoBM.innerHTML = valorActual;
+});
+
+//CHECK DE DISPLAY DE TEXTO SUPERIOR E INFERIOR
+const displayTM = document.getElementById("displayTM");
+const displayBM = document.getElementById("displayBM");
+
+displayTM.addEventListener("click", () => {
+  textoTM.classList.toggle("ocultar");
+});
+displayBM.addEventListener("click", () => {
+  textoBM.classList.toggle("ocultar");
+});
+
+//FONDO TRANSPARENTE TEXTO SUPERIOR E INFERIOR
+const fondoTransCheck = document.getElementById("fondoTransCheck");
+
+fondoTransCheck.addEventListener("click", () => {
+  textoTM.classList.toggle("fondoTrans");
+  textoBM.classList.toggle("fondoTrans");
+});
