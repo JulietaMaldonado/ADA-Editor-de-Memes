5f155f7870bb54465fdef1190ee32a4358051a28
Julieta Maldonado <80077281+JulietaMaldonado@users.noreply.github.com>
2022-07-07 15:15:33 -0300

    filtros, descarga y nuevos arreglos de codigo

--- main.js
+++ main.js
@@ -41,11 +41,6 @@ urlImagen.addEventListener("input", () => {
   meme.style = `background-image: url(${urlImagen.value})`;
 });
 
-/*urlImagen.addEventListener("input", (event) => {
-  const valorActual = event.target.value;
-  meme.style.backgroundImage = url(valorActual);
-});*/
-
 //COLOR DE MEME INPUT
 const colorFondoImagen = document.getElementById("colorFondoImagen");
 
@@ -53,6 +48,39 @@ colorFondoImagen.addEventListener("input", (event) => {
   const valorActual = event.target.value;
   meme.style.backgroundColor = valorActual;
 });
+
+//FILTROS DE IMAGEN
+let fbri = document.getElementById("fbri");
+let fopa = document.getElementById("fopa");
+let fcon = document.getElementById("fcon");
+let fdes = document.getElementById("fdes");
+let fgri = document.getElementById("fgri");
+let fsep = document.getElementById("fsep");
+let fhue = document.getElementById("fhue");
+let fsat = document.getElementById("fsat");
+let fneg = document.getElementById("fneg");
+
+const filtros = () => {
+  meme.style.filter = `brightness(${fbri.value}) 
+  opacity(${fopa.value}) 
+  contrast(${fcon.value}%) 
+  blur(${fdes.value}px)
+  greyscale(${fgri.value}%)
+  sepia(${fsep.value}%)
+  hue-rotation(${fhue.value}deg)
+  saturation(${fsat.value}%)
+  invert(${fneg.value})`;
+};
+filtros();
+
+fbri.addEventListener("input", filtros);
+fopa.addEventListener("input", filtros);
+fcon.addEventListener("input", filtros);
+fgri.addEventListener("input", filtros);
+fsep.addEventListener("input", filtros);
+fhue.addEventListener("input", filtros);
+fsat.addEventListener("input", filtros);
+fneg.addEventListener("input", filtros);
 
 //FUNCIONES DEL ASIDE "TEXTO"
 
@@ -77,16 +105,78 @@ const displayTM = document.getElementById("displayTM");
 const displayBM = document.getElementById("displayBM");
 
 displayTM.addEventListener("click", () => {
-  textoTM.classList.toggle("ocultar");
+  textoFondoTM.classList.toggle("ocultar");
 });
 displayBM.addEventListener("click", () => {
-  textoBM.classList.toggle("ocultar");
+  textoFondoBM.classList.toggle("ocultar");
+});
+
+//SELECT FUENTES
+
+const tiposFuentes = document.getElementById("tiposFuentes");
+
+tiposFuentes.addEventListener("input", (event) => {
+  console.log(event);
+  textoTM.style.fontFamily = event.target.value;
+  textoBM.style.fontFamily = event.target.value;
+});
+
+//INPUT DE COLOR DE TEXTO Y COLOR DE FONTO DE LOS TEXTOS SUPERIOR E INFERIOR
+
+const colorTextoInput = document.getElementById("colorTextoInput");
+const colorFondoInput = document.getElementById("colorFondoInput");
+const textoFondoTM = document.getElementById("textoFondoTM");
+const textoFondoBM = document.getElementById("textoFondoBM");
+
+colorTextoInput.addEventListener("input", (event) => {
+  textoTM.style.color = event.target.value;
+  textoBM.style.color = event.target.value;
+});
+colorFondoInput.addEventListener("input", (event) => {
+  textoFondoTM.style.backgroundColor = event.target.value;
+  textoFondoBM.style.backgroundColor = event.target.value;
 });
 
 //FONDO TRANSPARENTE TEXTO SUPERIOR E INFERIOR
 const fondoTransCheck = document.getElementById("fondoTransCheck");
 
 fondoTransCheck.addEventListener("click", () => {
-  textoTM.classList.toggle("fondoTrans");
-  textoBM.classList.toggle("fondoTrans");
+  textoFondoTM.classList.toggle("fondoTrans");
+  textoFondoBM.classList.toggle("fondoTrans");
 });
+
+//FUENTE TAMANIO CAMBIO
+
+const fuenteTam = document.getElementById("fuenteTam");
+
+fuenteTam.addEventListener("input", (event) => {
+  textoTM.style.fontSize = event.target.value;
+  textoBM.style.fontSize = event.target.value;
+});
+
+//ALINEAR TEXTO SUPERIOR E INFERIOR
+const aliIzquierda = document.getElementById("aliIzquierda");
+const aliCentro = document.getElementById("aliCentro");
+const aliDerecha = document.getElementById("aliDerecha");
+
+aliIzquierda.addEventListener("click", () => {
+  textoTM.style.textAlign = "left";
+  textoBM.style.textAlign = "left";
+});
+aliCentro.addEventListener("click", () => {
+  textoTM.style.textAlign = "center";
+  textoBM.style.textAlign = "center";
+});
+aliDerecha.addEventListener("click", () => {
+  textoTM.style.textAlign = "right";
+  textoBM.style.textAlign = "right";
+});
+
+//BOTON DE DESCARGA
+const descarga = document.getElementById("descarga");
+
+descarga.addEventListener("click", () =>
+  domtoimage
+    .toBlob(document.getElementById("meme"))
+    .then((blob) => saveAs(blob, "mi-meme.png"))
+);
